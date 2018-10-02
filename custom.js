4d82aa90a013adb64667f6b65578c9977f9591d1
windowsboot <windowsboot@hotmail.nl>
2018-10-02 10:42:10 +0200

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,26 @@
+var Hapi = require('hapi'),
+	nt = require("./notesto.js");
+	const loadJsonFile = require('load-json-file');
+
+(function(nt){
+
+	//$.DELETE('localhost:9999/reset');
+
+	nt.xPOSTjson('localhost:9999/create',
+		`{	name: 'databaseJson',
+			port: 10099,
+			version: 1,
+			init: () => { $.trace(1,'databaseJSON$init:1')
+		},
+			POST: {
+				"customer": ($) => {$.trace(1,'databaseJson$database:1')
+					jsn.loadJsonFile('data.json').then(json => {
+					console.log(json)
+					return JSON.stringify(json);
+				});
+			}
+			},
+		}`)
+	.then(nt.expect(/^SubServer.*$/,'Start a Subserver'))
+
+}(nt));
